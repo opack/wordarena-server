4519c345fceceecf9718d1d8b243cb3ef425b345
Opack <github@valdera.fr>
2016-08-19 14:00:18 +0200

    MOD : Scripts de préparation de machine

--- distribution/src/main/resources/admin/prepare_new_machine.sh
+++ distribution/src/main/resources/admin/prepare_new_machine.sh
@@ -2,11 +2,11 @@
 
 # Download all scripts
 wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/create_user_wordarena.sh
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/master/dist/admin/install_java.sh
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/master/dist/admin/install_mongodb.sh
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/master/dist/admin/setup_git.sh
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/master/dist/admin/fetch_liv.sh
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/master/dist/admin/install_wordarena.sh
+wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_java.sh
+wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_mongodb.sh
+wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/setup_git.sh
+wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/fetch_liv.sh
+wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_wordarena.sh
 
 # Make all scripts executable
 find . -name "*.sh" -exec chmod +x {} \;
