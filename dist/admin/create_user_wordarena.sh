d9f16b03da2a835c74b2945359c591baf85435f3
Opack <github@valdera.fr>
2016-08-19 14:06:21 +0200

    MOD : Scripts de préparation de machine

---
+++ dist/admin/create_user_wordarena.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Create a wordarena user
+# Source : https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-16-04
+# Exécuter en tant que root
+
+WORDARENA_USER=wordarena
+WORDARENA_PASSWORD=JeSuisWordArena!33
+
+# Create the user wordarena
+useradd $WORDARENA_USER --create-home
+echo $WORDARENA_USER:$WORDARENA_PASSWORD | chpasswd
+
+# Authorize this user to run sudo commands
+usermod -aG sudo $WORDARENA_USER
