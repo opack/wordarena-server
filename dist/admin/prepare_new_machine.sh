edaef5890955382c4e3e7d357b79026daca32516
Opack <github@valdera.fr>
2016-08-22 14:57:13 +0200

    MOD : Scripts de préparation de machine

--- dist/admin/prepare_new_machine.sh
+++ dist/admin/prepare_new_machine.sh
@@ -5,31 +5,32 @@
 #chmod +x create_user_wordarena.sh
 #./create_user_wordarena.sh
 #sudo -i -u wordarena
+curl -sSL https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/create_user_wordarena.sh | sh
 
 # Download all scripts
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_java.sh
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_mongodb.sh
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/setup_git.sh
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/fetch_dist.sh
-wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_wordarena.sh
+#wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_java.sh
+#wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_mongodb.sh
+#wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/setup_git.sh
+#wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/fetch_dist.sh
+#wget https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_wordarena.sh
 
 # Make all scripts executable
-find . -name "*.sh" -exec chmod +x {} \;
+#find . -name "*.sh" -exec chmod +x {} \;
 
 # Install Java
-./install_java.sh
+curl -sSL https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_java.sh | sh
 
 # Configure git
-./setup_git.sh
+curl -sSL https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/setup_git.sh | sh
 
 # Fetch wordarena distro
-./fetch_dist.sh
+curl -sSL https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/fetch_dist.sh | sh
 
 # Install MongoDB
-./install_mongodb.sh
+curl -sSL https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_mongodb.sh | sh
 
 # Install Wordarena service
-./install_wordarena.sh
+curl -sSL https://raw.githubusercontent.com/opack/wordarena-server/master/dist/admin/install_wordarena.sh | sh
 
 # Exit sudo wordarena
-exit
+#exit
--- distribution/src/main/resources/admin/create_user_wordarena.sh
+++ distribution/src/main/resources/admin/create_user_wordarena.sh
@@ -3,12 +3,13 @@
 # Source : https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-16-04
 # Exécuter en tant que root
 
-WORDARENA_USER=wordarena
-WORDARENA_PASSWORD=JeSuisWordArena!33
+#WORDARENA_USER=wordarena
+#WORDARENA_PASSWORD=JeSuisWordArena!33
 
 # Create the user wordarena
-useradd $WORDARENA_USER --create-home
-echo $WORDARENA_USER:$WORDARENA_PASSWORD | chpasswd
+#useradd $WORDARENA_USER --create-home
+#echo $WORDARENA_USER:$WORDARENA_PASSWORD | chpasswd
+adduser wordarena
 
 # Authorize this user to run sudo commands
 usermod -aG sudo $WORDARENA_USER
