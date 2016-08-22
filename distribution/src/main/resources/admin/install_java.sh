5d7d17dbad4d04163bf538a88820a3ea9099cce2
Opack <github@valdera.fr>
2016-08-22 10:27:18 +0200

    MOD : Scripts de préparation de machine

--- distribution/src/main/resources/admin/install_java.sh
+++ distribution/src/main/resources/admin/install_java.sh
@@ -3,4 +3,4 @@
 # Source : https://cloud.digitalocean.com/support/suggestions?article=how-to-install-java-on-ubuntu-with-apt-get&page=0&query=install%20java%208%20ubuntu%2016
 
 sudo apt-get update
-sudo apt-get install default-jre
+sudo apt-get install -y default-jre
